8c1c22e76df6fbe04a2fcdc551c2f48af7f9e4de
ryanbrowne360 <ryanbrowne360@gmail.com>
2020-10-05 11:19:07 -0400

    added most up to date file prior to presenting

--- script.js
+++ script.js
@@ -8,28 +8,36 @@ var devicePos;
 function buildYelpURL(position){
     var queryURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?";
     var params = [];
-    //For user to input location
-    //if($("#").val() != "") params.push(`location=${$("#").val()}`);
+    var terms = [];
+    //Checking if there is any input from user
+    if($("#location").val() != "") params.push(`location=${$("#location").val()}`);
     if(position != null) {
         params.push(`latitude=${position.coords.latitude}`);
         params.push(`longitude=${position.coords.longitude}`);
     }
-    if($("#catagories").val() != "Select a Category...") params.push(`catagories=${$("#catagories").val()}`);
-    return queryURL + params.join("&");
+    //Pushing categories and keywords onto terms
+    if($("#categories").val() != "Select a Category...") terms.push($("#categories").val());
+    if($("#keywords").val() != "") terms.push($("#keywords").val());
+    params.push(`term=${terms.join(",")}`); //Joining all contents in terms into 1 string and push onto params
+    return queryURL + params.join("&"); //return and build the query URL
 }
 
 //Send request to Yelp Fusion API
 function getYelpResults(){
+    //Using buildYelpURL() to build the query URL
     var queryURL = buildYelpURL(devicePos);
     $.ajax({
         url: queryURL,
         method: "GET",
-        asyn omain: true,
+        async: true,
+        crossDomain: true,
         headers: {
             "Authorization": `Bearer ${apiKeyYELP}`
         }
     }).then(function(response){
         console.log(response);
+        $(".results-container").css("display", "block");
+        buildCards(response.businesses);
     //In case unexpected errors happen
     }).fail(function(xhr, textStatus, error){
         console.log(xhr);
@@ -37,9 +45,56 @@ function getYelpResults(){
     });
 }
 
+//Build results cards
+function buildCards(businesses){
+    //Remove all cards under 1st and 2nd row of results
+    $("#1stRow, #2ndRow").empty();
+    //Building cards for 1st row
+    for(let i=0; i < 3; i++){
+        //In case there is more than 1 category, all categories are pushed onto an array then joined in append()
+        var categories = [];
+        for (let j=0; j < businesses[i].categories.length; j++){
+            categories.push(businesses[i].categories[j].title);
+        }
+        //Inserting contents for card
+        $("#1stRow").append(`
+              <div class="card col-md-4 col-lg-4 col-sm-4 col-xs-12">
+                  <img class="card-img-top" src="${businesses[i].image_url}" alt="Rest. img">
+                  <div class="card-body">
+                      <h5>${businesses[i].name}</h5>
+                      <p><small class="text-muted">${categories.join(", ")}</small></p>
+                      <p>${businesses[i].location.display_address.join(",")}</p>
+                      <p>${businesses[i].phone}</p>
+                  </div>
+              </div>
+        `);
+    }
+    //Building cards for 2nd row
+    for(let i=3; i < 6; i++){
+        //In case there is more than 1 category, all categories are pushed onto an array then joined in append()
+        var categories = [];
+        for (let j=0; j < businesses[i].categories.length; j++){
+            categories.push(businesses[i].categories[j].title);
+        }
+        //Inserting contents
+        $("#2ndRow").append(`
+              <div class="card col-md-4 col-lg-4 col-sm-4 col-xs-12">
+                  <img class="card-img-top" src="${businesses[i].image_url}" alt="Rest. img">
+                  <div class="card-body">
+                      <h5>${businesses[i].name}</h5>
+                      <p><small class="text-muted">${categories.join(", ")}</small></p>
+                      <p>${businesses[i].location.display_address.join(",")}</p>
+                      <p>${businesses[i].phone}</p>
+                  </div>
+              </div>
+        `);
+    }
+
+}
+
 //Using native HTML5 geolocation to get user's location and pass to getRestaurant()
 function getLocation(){
-    console.log("getLocation() Ran")
+    console.log("getLocation() Ran");
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(setPosition);
     } else { 
@@ -47,6 +102,7 @@ function getLocation(){
     }
 }
 
+//In order to work with getCUrrentPosition()
 function setPosition(position){
   devicePos = position;
 }
